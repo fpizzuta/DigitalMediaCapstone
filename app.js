b06f20612e676030c1eb6554f8b95720e5bc1f90
Frank Pizzuta <frank@pizzuta.com>
2019-10-05 16:36:39 -0400

    basic web service running at /drwho

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+var pinball = require('./routes/pinball');
+
+app.use('/drwho',pinball);
+
+app.use((req, res, next)=>{
+    res.status(404);
+    res.send("404 Sorry, this file cannot be found");
+  });
+
+// app.listen(8080,'0.0.0.0', () => console.log('Example app listening on port 8080!'))
+module.exports = app;
