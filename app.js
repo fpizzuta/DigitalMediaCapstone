410aa09dbf7b3d447b6719ddd4430e9d7152f57c
fpizzuta <frank@pizzuta.com>
2019-10-06 17:30:37 -0400

    websocket client connect working on localhost

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var pinball = require('./routes/pinball');
 
 app.use('/drwho',pinball);
 
+
 app.use((req, res, next)=>{
     res.status(404);
     res.send("404 Sorry, this file cannot be found");
--- routes/pinball.js
+++ routes/pinball.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.post('/', (req, res, next)=>{
  console.log('/drwho requested');
  
 });
+
+router.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
 
 module.exports = router;
